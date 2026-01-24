ff528ed58a1c3cad5184272200e4cc1914f4a5c1
Alexey <alexeyriabinin@gmail.com>
2026-01-24 20:48:27 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import "/src/sass/style.scss";
+ "use strict"; //работаем в современном режиме
+
+let incr = 10,
+decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+
+console.log(5%2);
+
+console.log(2*5 == 8);
+const isChecked = true,
+      isClose = false;
+
+ console.log(isChecked && isClose);
