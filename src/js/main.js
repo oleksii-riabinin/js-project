2628844f033b0745bd723a30f766f0aa099f1a79
Alexey <alexeyriabinin@gmail.com>
2026-01-30 23:51:18 +0200

    практика на освоение 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,78 +1,50 @@
 import "/src/sass/style.scss";
  "use strict"; //работаем в современном режиме
 
-// const numberr0fFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberr0fFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// const personalMovieDB = {
-//   movies: {
+const personalMovieDB = {
+  movies: {
 
-//   },
+  },
   
-//   actors:{
+  actors:{
 
-//   },
+  },
 
-//   genres: [],
+  genres: [],
 
-//   privat:false,
+  privat:false,
 
-//   count:numberr0fFilms
-// };
-
-// const a = prompt("Один из последних просмотренных фильмов?",""),
-//       b = prompt("Оцените его?", ""),
-//       c = prompt("Один из последних просмотренных фильмов?",""),
-//       d = prompt("Оцените его?", "");
-
-//      personalMovieDB.movies[a] = b;
-//      personalMovieDB.movies[c] = d;
-
-//      console.log(personalMovieDB);
+  count:numberr0fFilms
+};
 
 
+     for(let i = 0;i<2;i++){
+      const a = prompt("Один из последних просмотренных фильмов?",""),
+      b = prompt("Оцените его?", "");
 
-function firstTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
-    // Пишем решение вот тут
-    for(let i = 0; i<arr.length; i++){
-      result[i]=arr[i];
-    }
-    console.log(result);
-    
-    // Не трогаем
-    return result;
-}
-firstTask()
-
-function secondTask() {
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-
-    for(let i = 0; i < data.length; i++){
-      if(typeof(data[i])===`number`){
-        data[i]= data[i]*2;
+      if(a != null && b != null && a != "" && b != "" && a.length<50){
+          personalMovieDB.movies[a] = b;
+          console.log('done')
+      }else{
+        console.log("error")
+        i--;
       }
-      else if(typeof(data[i])===`string`){
-        data[i]=`${data[i]} - done`;
-      }
-    }
-    console.log(data);
-    return data;
-}
+       
+     }
 
-secondTask();
+     if(personalMovieDB.count<10){
+      console.log("Просмотрено мало фильмов");
+     }else if(personalMovieDB.count<=10 && personalMovieDB.count<30){
+      console.log("Вы класический зритель");
+     }else if(personalMovieDB.count<=30){
+      console.log("Вы киноман");
+     }else{
+      console.log("Ошибка");
+     }
+      
+     console.log(personalMovieDB);
 
-function thirdTask() {
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
 
-    for (let i = 1; i <= data.length; i++) {
-        result[i - 1] = data[data.length - i]
-    }
 
-    console.log(result);
-    return result;
-}
-
-thirdTask();
