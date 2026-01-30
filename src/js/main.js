5b715c41d637396ec22f74c093f44107e3075eda
Alexey <alexeyriabinin@gmail.com>
2026-01-30 13:53:33 +0200

    практика циклы

--- src/js/main.js
+++ src/js/main.js
@@ -30,30 +30,70 @@ import "/src/sass/style.scss";
 //      console.log(personalMovieDB);
 
 
-let result = "";
-const length = 7;
 
-for (let i = 1; i <length; i++){
 
-  for (let j = 0; j<i; j++){
-    result += "*";
+
+function firstTask() {
+    // Пишем решение вот тут
+    for(let i = 5; i<=10; i++){
+      console.log(i);
+    } 
+}
+firstTask()
+
+function secondTask() {
+    // Пишем решение вот тут
+    for (let i = 20; i>10; i--){
+      if (i===13) break;
+      console.log(i);
+    
+    }
+    
+}
+secondTask()
+
+function thirdTask() {
+    // Пишем решение вот тут
+    for (let i = 2; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
+thirdTask()
+
+
+function fourthTask() {
+    let i = 2;
+
+  while (i <= 16) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+    i++;
+}
+}
+fourthTask()
+
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    for (let i = 5; i <= 10; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
   }
 
-  result += "\n";
+fifthTask()
+
+
+const arr = [];
+
+for (let i = 0; i < 5; i++) {
+    arr[i] = i + 1;
 }
 
-console.log(result);
-
-
-
-first: for (let i = 0; i <3; i++){
-console.log(`First level: ${i}`);
-  for (let j = 0; j < 3; j++){
- console.log(`Second level: ${j}`);
-  
-   for (let k = 0; k < 3; k++){
-    if(k===2) continue first;
-console.log(`Third level: ${k}`);
-  }
-}
-}
+console.log(arr);
