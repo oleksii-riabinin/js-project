911a19624e5e1d9872e4316a79abbf610bc5e5dd
Alexey <alexeyriabinin@gmail.com>
2026-02-01 14:32:35 +0200

    практика на освоение 2(через цикл while)

--- src/js/main.js
+++ src/js/main.js
@@ -19,11 +19,11 @@ const personalMovieDB = {
   count:numberr0fFilms
 };
 
-
-     for(let i = 0;i<2;i++){
+    let i = 0;
+     while(i<2){
       const a = prompt("Один из последних просмотренных фильмов?",""),
       b = prompt("Оцените его?", "");
-
+      i++
       if(a != null && b != null && a != "" && b != "" && a.length<50){
           personalMovieDB.movies[a] = b;
           console.log('done')
