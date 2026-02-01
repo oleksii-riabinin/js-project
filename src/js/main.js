640289867aca7389efdd751c0f7931ab1aa329d2
Alexey <alexeyriabinin@gmail.com>
2026-02-01 15:25:10 +0200

    виды функций

--- src/js/main.js
+++ src/js/main.js
@@ -1,51 +1,61 @@
 import "/src/sass/style.scss";
  "use strict"; //работаем в современном режиме
 
-const numberr0fFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberr0fFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-  movies: {
+// const personalMovieDB = {
+//   movies: {
 
-  },
+//   },
   
-  actors:{
+//   actors:{
 
-  },
+//   },
 
-  genres: [],
+//   genres: [],
 
-  privat:false,
+//   privat:false,
 
-  count:numberr0fFilms
+//   count:numberr0fFilms
+// };
+
+    
+//      for(let i = 0;i<2;i++){
+//       const a = prompt("Один из последних просмотренных фильмов?",""),
+//       b = prompt("Оцените его?", "");
+
+//       if(a != null && b != null && a != "" && b != "" && a.length<50){
+//           personalMovieDB.movies[a] = b;
+//           console.log('done')
+//       }else{
+//         console.log("error")
+//         i--;
+//       }
+//      }
+
+//      if(personalMovieDB.count<10){
+//       console.log("Просмотрено мало фильмов");
+//      }else if(personalMovieDB.count<=10 && personalMovieDB.count<30){
+//       console.log("Вы класический зритель");
+//      }else if(personalMovieDB.count<=30){
+//       console.log("Вы киноман");
+//      }else{
+//       console.log("Ошибка");
+//      }
+//      console.log(personalMovieDB);
+
+
+function declaration (){
+  console.log("declaration");
+}
+declaration ();
+
+const expression = function(){
+console.log("expression");
 };
+expression();
 
-    let i = 0;
-     do{
-      const a = prompt("Один из последних просмотренных фильмов?",""),
-      b = prompt("Оцените его?", "");
-      i++
-      if(a != null && b != null && a != "" && b != "" && a.length<50){
-          personalMovieDB.movies[a] = b;
-          console.log('done')
-      }else{
-        console.log("error")
-        i--;
-      }
-       
-     }
-     while(i<2);
-
-     if(personalMovieDB.count<10){
-      console.log("Просмотрено мало фильмов");
-     }else if(personalMovieDB.count<=10 && personalMovieDB.count<30){
-      console.log("Вы класический зритель");
-     }else if(personalMovieDB.count>=30){
-      console.log("Вы киноман");
-     }else{
-      console.log("Ошибка");
-     }
-      
-     console.log(personalMovieDB);
-
+const calc = ( a, b) => a+b;
+ console.log(calc(2,2));
 
 
