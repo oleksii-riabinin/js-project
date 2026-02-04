e10d017a7d17da7de089975632e975487bed8c42
Alexey <alexeyriabinin@gmail.com>
2026-02-04 15:13:44 +0200

    методы и свойство строк

--- src/js/main.js
+++ src/js/main.js
@@ -45,36 +45,36 @@ import "/src/sass/style.scss";
 //      console.log(personalMovieDB);
 
 
-function sayHello(name) {
-    return `Привет, ${name}!`;
-}
 
-console.log(sayHello("Alexey"));
+//свойство поиска размера
+const row = "test";
+console.log(row.length);
+const arr = [ 1,2,3];
+console.log(arr.length);
 
-function returnNeighboringNumbers (num) {
-console.log([num-1,num,num+1]);
+//методы которые меняют строку на верхний или нижний регистры
+const strUp = "test";
+console.log(strUp.toUpperCase());
+const strLower = "TEST";
+console.log(strLower.toLowerCase());
 
-}
-returnNeighboringNumbers (6);
+//поиск начала(индекс) части строки ИЛИ поиск подстроки 
+let fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
 
-function getMathResult(base,count) {
-    if (typeof count !== 'number' || count <= 0) {
-        return base;
-    }
+// методы которые обрезают часть строки 
+const logg = "Hello world and Alexey";
+console.log(logg.slice(6,15));
+console.log(logg.substring(6,15));
 
-    let result = '';
+// Математическая библиотека( метод скругления числа )
+const num =12.2;
+console.log(Math.round(num));
 
-    for (let i = 1; i <= count; i++) {
-        result += base * i;
-
-        if (i !== count) {
-            result += '---';
-        }
-    }
-
-    return result;
-}
-console.log(getMathResult(5,3));
-
-
+//методы котрые меняют строку га числовой элемент
+//1. Округляет число 
+//2. Не округляет( может вывести дробное число )
+const test ="12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
 
