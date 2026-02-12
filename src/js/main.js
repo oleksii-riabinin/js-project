56856d487ad0c338f76bfd135dd42306b9bb3564
Alexey <alexeyriabinin@gmail.com>
2026-02-12 21:52:38 +0200

    масиви, методи масивів

--- src/js/main.js
+++ src/js/main.js
@@ -79,35 +79,44 @@ import "/src/sass/style.scss";
 //      writeYourGenres();
 
 
-const options = {
-    name: "test",
-    width: 1024,
-    height: 1024,
-    colors:{
-        border:"black",
-        bg:"red"
-    },
-    makeTest: function(){
-        console.log("TEst")
-    }
-};
-const{border, bg}= options.colors;
-console.log(border);
-options.makeTest();
- console.log(Object.keys(options).length);
- console.log(Object.keys(options));
-let counter = 0;
-for (let key in options) {
-    if (typeof options[key] === "object") {
-        for (let i in options[key]) {
-            console.log(`свойство ${i} имеет значение ${options[key][i]}`);
-            counter++
-        }
-    } else {
-        console.log(`свойство ${key} имеет значение ${options[key]}`);
-        counter++
-    }
-}
-console.log(counter);
+let arr = [1,2,3,6,8];
 
- 
+//удаляет последний єлемент масива
+arr.pop();
+
+// // добавление єлемента в конец масива
+arr.push(10);
+console.log(arr);
+
+// //перебор значений в масиве
+for(let i =0;i<arr.length;i++){
+    console.log(arr[i]);
+}
+// //или(не работает в об'ектах)
+for(let value of arr){
+    console.log(value);
+}
+
+//гибкий перебор элементов
+arr.forEach(function(item,i,arr){
+    console.log(`${i}: ${item} внутри масива ${arr}`);
+});
+
+const str = "pivo, sneki, voda, 99";
+//метод который показывает через какой знак пишем элементы 
+const products = str.split(",");
+//метод который меняет знак при выводе элементов 
+console.log(products.join(";"));
+
+//метод который сщртирует элементы в масиве(хорощо работает со строчными)но может и с другими типами
+ const sortirovka = ["a","c","e", "b", 0, "B"];
+ sortirovka.sort();
+ console.log(sortirovka);
+
+//шаблон котрый работает с сортировкой других типов в масиве
+const sortOthersType = [3,6,1,10];
+sortOthersType.sort(compareNum);
+console.log(sortOthersType);
+function compareNum(a,b){
+    return a-b;
+}
