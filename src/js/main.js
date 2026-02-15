a4e4954ebbdd29c030477df9da36dbb3d3c4e5a6
Alexey <alexeyriabinin@gmail.com>
2026-02-16 01:16:03 +0200

    практика на масиви 2

--- src/js/main.js
+++ src/js/main.js
@@ -96,3 +96,69 @@ function standardizeStrings(arr) {
 }
 standardizeStrings(favoriteCities);
 
+//Метод який змінює порядок елементів в масиві(Тільки з масивами!)
+let arr = ["aa","b", "c"];
+let arrRevers = arr.reverse();
+console.log(arrRevers);
+
+//функція яка повертає рядок в зворотньому напрямку(через цикл).
+const someString = 'This is some strange string';
+function reverse(str) {
+    let result = "";
+if(typeof str !== "string"){
+    return "Ошибка!"
+}
+for(let i = str.length-1; i>=0; i--){
+result+= str[i];
+}
+return result;
+}
+console.log(reverse(someString));
+
+//функція яка повертає рядок в зворотньому напрямку(через метод reverse()).
+const someStringMas = 'This is some strange string';
+function reverse(str) {
+    if (typeof str !== "string") {
+        return "Ошибка!";
+    }
+    return str
+        .split("")   // робимо масив символів з рядку
+        .reverse()   // перевертаємо масив
+        .join("");   // збираємо назад у рядок
+}
+console.log(reverse(someStringMas));
+
+
+//створюємо масиви валют 
+const baseCurrencies = ['USD', 'EUR', 'USD'];
+const additionalCurrencies = ['UAH', 'AAA', 'CNY'];
+//функція приймає 1. два масива валют, 2. валюту якої не повинно бути при виводі(не обов'язково )
+function availableCurr(arr, missingCurr) {
+let result = "Доступные валюты:\n";
+    if (arr.length === 0) {
+        return 'Нет доступных валют';
+    }
+    //через метод .filter робимо умову щоб при виводі виводились валюти які не дорівнюють валюти якої не повинно бути 
+    const filteredArr = arr.filter(function(curr) {
+        return curr !== missingCurr;
+    });
+    //працює до тих пір поки він не буде менше за відфільтровіні валюти і в результат додаються наші валюти які будуть переходити на новий рядок 
+    for (let i = 0; i < filteredArr.length; i++) {
+        result += filteredArr[i] + "\n";
+    }
+    return result;
+}
+//тут працює так що з початку виводяться значення з першого масиву, потім з другого і в кінці вже та валюта яку треба прибрати
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies],"AAA" ));
+
+
+//метод який перебирає елементи та повертає умові і якщо якийсь елемент не пройшов умову то його не виводять(.filter)
+const number = [1,6,8,3,9];
+const result = number.filter(function(nechet){
+    return nechet%2!==0;
+});
+console.log(result);
+//через стрілочну функцію
+const num = [1,2,5,6,8];
+const resultNum = num.filter(n=> n > 2);
+console.log(resultNum);
