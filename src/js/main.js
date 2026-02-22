35759f94561be7bc8e271fc3a11d00a1e2499efb
Alexey <alexeyriabinin@gmail.com>
2026-02-22 02:08:22 +0200

    практика на те чи вистачить нам грошей для покупки матеріалів на деякі магазини

--- src/js/main.js
+++ src/js/main.js
@@ -3,93 +3,138 @@ import "/src/sass/style.scss";
 
   
 
-        const personalMovieDB = {
-        count:0,
-        movies: {
+//         const personalMovieDB = {
+//         count:0,
+//         movies: {
 
-        },
-        actors:{
+//         },
+//         actors:{
 
-        },
-        genres: [],
-        privat:false,
-//зараз зробили наші функції як методи об'єкту
-        start: function(){
-        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
-        while (personalMovieDB.count=='' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
-        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-        },
+//         },
+//         genres: [],
+//         privat:false,
+// //зараз зробили наші функції як методи об'єкту
+//         start: function(){
+//         personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//         while (personalMovieDB.count=='' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+//         personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//     }
+//         },
 
-        rememberMyFilms: function(){
-          for(let i = 0;i<2;i++){
-      const a = prompt("Один из последних просмотренных фильмов?","").trim(),
-      b = prompt("Оцените его?", "");
+//         rememberMyFilms: function(){
+//           for(let i = 0;i<2;i++){
+//       const a = prompt("Один из последних просмотренных фильмов?","").trim(),
+//       b = prompt("Оцените его?", "");
 
-      if(a != null && b != null && a != "" && b != "" && a.length<50){
-          personalMovieDB.movies[a] = b;
-          console.log('done')
-      }else{
-        console.log("error")
-        i--;
-      }
-     }
-     },
+//       if(a != null && b != null && a != "" && b != "" && a.length<50){
+//           personalMovieDB.movies[a] = b;
+//           console.log('done')
+//       }else{
+//         console.log("error")
+//         i--;
+//       }
+//      }
+//      },
 
-     detectPersonalLevel:function(){
-  if(personalMovieDB.count<10){
-      console.log("Просмотрено мало фильмов");
-     }else if(personalMovieDB.count<=10 && personalMovieDB.count<30){
-      console.log("Вы класический зритель");
-     }else if(personalMovieDB.count<=30){
-      console.log("Вы киноман");
-     }else{
-      console.log("Ошибка");
-     }
-     },
+//      detectPersonalLevel:function(){
+//   if(personalMovieDB.count<10){
+//       console.log("Просмотрено мало фильмов");
+//      }else if(personalMovieDB.count<=10 && personalMovieDB.count<30){
+//       console.log("Вы класический зритель");
+//      }else if(personalMovieDB.count<=30){
+//       console.log("Вы киноман");
+//      }else{
+//       console.log("Ошибка");
+//      }
+//      },
 
-     showMyDB:function(hidden){
-        if(!hidden){
-           console.log(personalMovieDB);
-        }
-     },
+//      showMyDB:function(hidden){
+//         if(!hidden){
+//            console.log(personalMovieDB);
+//         }
+//      },
 
-     writeYourGenres:function(){
-        for(let i =1; i<=3;i++){
+//      writeYourGenres:function(){
+//         for(let i =1; i<=3;i++){
             
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-            if(genre == '' || genre == null){
-                console.log("Вы не ввели данные");
-                i--;
-            }else{
-            personalMovieDB.genres[i-1] = genre;
-            }
-        }
-        personalMovieDB.genres.forEach((item, i)=>{
-            console.log(`Любимый жанр ${i + 1} - это ${item}`);
-        });
-     },
-//зроби метод який буде перевіряти приватнійсть і якщо вона дорівнює false то виведе true і навпаки
-     toggleVisibleMyDB: function(){
-        if(personalMovieDB.privat){
-            personalMovieDB.privat= false;
-        }else{
-            personalMovieDB.privat = true;
-        }
-     }
-        };
+//             let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+//             if(genre == '' || genre == null){
+//                 console.log("Вы не ввели данные");
+//                 i--;
+//             }else{
+//             personalMovieDB.genres[i-1] = genre;
+//             }
+//         }
+//         personalMovieDB.genres.forEach((item, i)=>{
+//             console.log(`Любимый жанр ${i + 1} - это ${item}`);
+//         });
+//      },
+// //зроби метод який буде перевіряти приватнійсть і якщо вона дорівнює false то виведе true і навпаки
+//      toggleVisibleMyDB: function(){
+//         if(personalMovieDB.privat){
+//             personalMovieDB.privat= false;
+//         }else{
+//             personalMovieDB.privat = true;
+//         }
+//      }
+//         };
 
 
     
   
 
 
+   const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+}
+console.log(shoppingMallData);
+
+
+function isBudgetEnough(data) {
+
+    let totalArea = 0;
+
+    // 1. Рахуємо площу всіх магазинів
+    for (let i = 0; i < data.shops.length; i++) {
+        totalArea += data.shops[i].width * data.shops[i].length;
+    }
+
+    // 2. Обʼєм
+    const totalVolume = totalArea * data.height;
+
+    // 3. Вартість опалення
+    const totalCost = totalVolume * data.moneyPer1m3;
+
+    // 4. Перевірка бюджету
+    if (totalCost <= data.budget) {
+        return 'Бюджета достаточно';
+    } else {
+        return 'Бюджета недостаточно';
+    }
    
-
-
-
+}
     
-
+console.log(isBudgetEnough(shoppingMallData));
 
 
 
