9a0e5159925444757d4eeb06cbd4ab38ec0160d2
Alexey <alexeyriabinin@gmail.com>
2026-03-04 16:30:15 +0200

    вивчення як працювати з елементами на сторінці та основні методи для того як працювати з елементами

--- src/js/main.js
+++ src/js/main.js
@@ -2,171 +2,82 @@ import "/src/sass/style.scss";
  "use strict"; //работаем в современном режиме
 
   
+const box = document.getElementById("box"),
+      btns= document.getElementsByTagName("button"),
+      circles = document.getElementsByClassName("circle"),
+      hearts = document.querySelectorAll(".heart"),
+      oneHeart = document.querySelector(".heart"),
+      wrapper =document.querySelector(".wrapper");
 
-//         const personalMovieDB = {
-//         count:0,
-//         movies: {
-
-//         },
-//         actors:{
-
-//         },
-//         genres: [],
-//         privat:false,
-// //зараз зробили наші функції як методи об'єкту
-//         start: function(){
-//         personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
-//         while (personalMovieDB.count=='' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
-//         personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
-//     }
-//         },
-
-//         rememberMyFilms: function(){
-//           for(let i = 0;i<2;i++){
-//       const a = prompt("Один из последних просмотренных фильмов?","").trim(),
-//       b = prompt("Оцените его?", "");
-
-//       if(a != null && b != null && a != "" && b != "" && a.length<50){
-//           personalMovieDB.movies[a] = b;
-//           console.log('done')
-//       }else{
-//         console.log("error")
-//         i--;
-//       }
-//      }
-//      },
-
-//      detectPersonalLevel:function(){
-//   if(personalMovieDB.count<10){
-//       console.log("Просмотрено мало фильмов");
-//      }else if(personalMovieDB.count<=10 && personalMovieDB.count<30){
-//       console.log("Вы класический зритель");
-//      }else if(personalMovieDB.count<=30){
-//       console.log("Вы киноман");
-//      }else{
-//       console.log("Ошибка");
-//      }
-//      },
-
-//      showMyDB:function(hidden){
-//         if(!hidden){
-//            console.log(personalMovieDB);
-//         }
-//      },
-
-//      writeYourGenres:function(){
-//         for(let i =1; i<=3;i++){
-            
-//             let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-//             if(genre == '' || genre == null){
-//                 console.log("Вы не ввели данные");
-//                 i--;
-//             }else{
-//             personalMovieDB.genres[i-1] = genre;
-//             }
-//         }
-//         personalMovieDB.genres.forEach((item, i)=>{
-//             console.log(`Любимый жанр ${i + 1} - это ${item}`);
-//         });
-//      },
-// //зроби метод який буде перевіряти приватнійсть і якщо вона дорівнює false то виведе true і навпаки
-//      toggleVisibleMyDB: function(){
-//         if(personalMovieDB.privat){
-//             personalMovieDB.privat= false;
-//         }else{
-//             personalMovieDB.privat = true;
-//         }
-//      }
-//         };
-
-
-    
+    //працювання з елементами на сторінці
+    //коли пишем значення яке хочемо змінити то пишемо не як в css(-) а camelCase
+  box.style.backgroundColor = "blue";
+  box.style.width = "500px";
   
-// const restorantData = {
-//     menu: [
-//         {
-//             name: 'Salad Caesar',
-//             price: '14$'
-//         },
-//         {
-//             name: 'Pizza Diavola',
-//             price: '9$'
-//         },
-//         {
-//             name: 'Beefsteak',
-//             price: '17$'
-//         },
-//         {
-//             name: 'Napoleon',
-//             price: '7$'
-//         }
-//     ],
-//     waitors: [
-//         {name: 'Alice', age: 22}, {name: 'John', age: 24}
-//     ],
-//     averageLunchPrice: '20$',
-//     openNow: true
-// };
+  //змінює одразу декілька значеннь і тут пишемо як в звичайному css
+  box.style.cssText = "background-color:blue; width:500px";
 
+  //коли працюємо з псевдомасивами то тут вовинні обирати якийсь окремий елемент
+  btns[1].style.borderRadius = "100%";
 
-// function isOpen(prop) {
-//     let answer = '';
-//     prop ? answer = 'Открыто' : answer = 'Закрыто';
-//     return answer;
-// }
-// console.log(isOpen(restorantData.openNow))
+  //змінення стилів для декількох елементів
+  //1) через цикл for
+ for(let i = 0; i < hearts.length;i++){
+  hearts[i].style.backgroundColor="green";
+ }
+//2) через метод forEach
+ hearts.forEach(item =>{
+  item.style.backgroundColor="green";
+ });
 
-// //створюємо фунцію яка буде показувати чи менше або більше сумма деяких двої товарів
-// function isAverageLunchPriceTrue(fDish, sDish, average) {
-//     //тут ми усюди додаємо спереду "+"щоб можно було пистаи числові значення а не рядки
-//     //потім метод .slice мии прибираємо перший елемент та останній залишаючи тільки число 
-//     if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < +average.slice(0,-1)) {
-//         return 'Цена ниже средней';
-//     } else {
-//         return 'Цена выше средней';
-//     }
-// }
-// //тут ми вже наші значення це з який масивів ми будемо брати числа
-// console.log(isAverageLunchPriceTrue(restorantData.menu[1], restorantData.menu[3], restorantData.averageLunchPrice));
+ //основні методи для працювання з елементами на сторінці
 
+ //створюємо нові елементи(він існує тільки в js)
+ const div =document.createElement("div");
 
+//додає до нашого елементу класс
+div.classList.add("black");
 
-// function transferWaitors(data) {
-//     const copy = Object.assign({}, data);
+//додає вже елемент в кінець тегу body,щоб його було видно на сторінці
+document.body.append(div);
 
-//     // Нам просто нужно менять весь массив данных,
-//     // а не лезть напрямую менять каждого из сотрудников
-//     // Так как это верхний уровень объекта, то значение 
-//     // будет меняться только у копии
-//     copy.waitors = [{name: 'Mike', age: 32}];
-//     return copy;
-// }
+//додали наш блок в огортку wrapper
+wrapper.append(div);
 
-// console.log(transferWaitors(restorantData));
+//додає елемент в початок батьківського елемента
+wrapper.prepend(div);
 
+//додає елемент перед якимось елементом
+//елемент буде перед першим сердчком
+hearts[0].before(div);
 
+//додає елемент після якогось елемента
+//елемент буде після першого сердчка
+hearts[0].after(div);
 
+//видаляє якийсь елемент
+ circles[0].remove();
 
-//отримай елемент по id(все що з document працює тільки в браузері)
-const box = document.getElementById("box");
-console.log(box);
+//замінюємо якийсь елемент на інший
+//тут сердечко замінили на кружочок
+hearts[0].replaceWith(circles[0]);
 
-//щоб отримати псевдомасив з елементів(html colection і він шукає всі елементи з назвою тегу)
-//  та якщо треба отримати якийсь окремий елемент просто шукаєм за індексом[]
-const btns = document.getElementsByTagName("button")[1];
-console.log(btns);
+//можемо вводити як текст так і HTML структуру
+div.innerHTML = "<h2>hello world</h2>";
 
-//виводить всі елементи з класом , також виводить(html colection)
-const circles = document.getElementsByClassName("circle");
-console.log(circles);
+//можемо вводити тільки текст
+div.textContent= "text";
 
-//виводить css селектори( він новіший за минулі),
-const hearts = document.querySelectorAll(".heart");
-//можно через forEach
-hearts.forEach(item=>{
-    console.log(item);
-});
+//додаємо Html структуру на сторінку
+//спочатку пишемо над яким елементом ми будемо працювати 
+//потім пишемо два значення 1.як вставимо, 2. що вставимо
 
-//виводить самий перший елемент
-const oneHeart = document.querySelector(".heart");
-console.log(oneHeart);
+//1) вставляємо перед елементом 
+div.insertAdjacentHTML("beforebegin","<h2>hello</h2>");
+//2)вставляємо на початок нашого елементу(як prepend) 
+div.insertAdjacentHTML("afterbegin","<h2>hello</h2>");
+//3) вставляємо в кінець нашого елементу
+div.insertAdjacentHTML("beforeend","<h2>hello</h2>");
+//4) вставляємо після елементу 
+div.insertAdjacentHTML("afterend","<h2>hello</h2>");
+
