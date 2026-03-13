85e2a52e79bc9cda16cf6da48cbb7d7290dc7c16
Alexey <alexeyriabinin@gmail.com>
2026-03-13 12:43:50 +0200

    вивчення рикурсії і приклади її використання

--- src/js/main.js
+++ src/js/main.js
@@ -1,72 +1,121 @@
 import "/src/sass/style.scss";
  "use strict"; //работаем в современном режиме
 
-  
-//події вони нам кажуть про те що щось відбулось на сторінці( клік, прокрут колеса миші ...)
-//обробник це функція яка працюватиме після того як подія відбулась
-
-const btn = document.querySelector('button');
-const overlay = document.querySelector(".overlay");
-//1) обробник натискання
-// ( але цей метод рідко використовують, 
-// бо коли написати схожу функцію але з друшим виводом 
-// то воно покаже тільки друге повідомлення)
-// btn.onclick = function(){
-//   alert('Click');
-// }
-
-//2) правильне написання обробників подій(перше значення назву події, а другим це callback функція)
-//цей метод краще бо ми можемо давати нашому обробнику декілька дій(тут він спочатку виведе click, а потім Second click)
-btn.addEventListener('click',()=>{
-alert('Click');
-});
-
-//тут буде виконуватись дія коли будемо наводити мишку на елемент
-btn.addEventListener('mouseenter',()=>{
-console.log("hover")
-});
-
-//об'єкт подій(те що йде в аргументі функції,завжди пишемо першим, а далі інші аргументи),також він показує що було зроблено з елементом
-btn.addEventListener('click',(event)=>{
-  //властивість яка показує що це за елемент .target
-console.log(event.target);
-//показує що за подія відбулась
-console.log(event.type);
-//воно змінить вивід нашого target( зверху буде дочірній елемент, а знизу батьківський)
-console.log(event.currentTarget);
-//при натисканні видаляємо елемент
-event.target.remove();
-});
-
-let i =0;
-//функція
-const deleteElement =(event)=>{
-console.log(event.target);
-i++;
-if(i==1){
-  //метод видалення
-  btn.removeEventListener("click", deleteElement);
+//Рекурсія - функція яка запускає сама себе поки не дійде до базового випадку
+function pow (x,n){
+  let result = 1;
+  for(let i =0;i<n;i++){
+    result*=x;
+  }
+  return result;
 }
+
+//пошук степення через рекурсію
+function pow (x,n){
+  //база
+if(n ===1){
+  return x;
+}else{
+  //2 * 2^3
+  return x* pow(x,n-1);
+}
+}
+console.log(pow(2,4));
+
+
+let students = {
+  js:[{
+    name:"John",
+    progress:100
+  },
+  {
+    name:"Ann",
+    progress:60
+  }],
+
+  html:{
+    basic:[{
+      name:"Peter",
+      progress:20
+    },
+    {
+      name:"Ann",
+      progress:18
+    }],
+    pro:[{
+      name:"Same",
+      progress:10
+    }],
+    //якщо додати не масив а об'єкт то перший метод не спрацює
+    //а спрацює тільки метод який був зробленний через рекурсію
+    semi:{
+      students:[{
+        name:"test",
+        progress:110
+      }]
+    }
+  
+  }
 };
-//тут ця функція буде працювати після натискання
-btn.addEventListener("click", deleteElement);
-//стоврили спливання подій, воно в нас виведе два target типу батьківсьокого елементу, а потім п виведе дочірнього елементу
 
-overlay.addEventListener("click", deleteElement);
+//пошук середнього прогрессу з усього об'єкту
+ function getTotalProgressByIteration(data){
+  let total =0;
+  let students =0;
+  //метод об'єктів який дозволяє отримати масив значеннь цього об'єкту(Object.value)
 
-//робимо стандартну відмову для браузера(типу не переходь за посилання, юо ми будемо робити щось інше)
-const link = document.querySelector("a");
-link.addEventListener("click", (e)=>{
-  //завжди ставимо на початок
-e.preventDefault();
-//ми не перейдемо за посиланням , а просто виведемо target в консоль
-console.log(e.target);
-});
+  //перебор кожного значення в об'єкті
+  for(let course of Object.values(data)){
+    //метод який повертає true якщо об'єкт є масивом 
+    //i false якщо ні (Array.isArray())
+    if(Array.isArray(course)){
+      //перебирає кількість студентів
+      students += course.length;
 
-//робимо наші події для декількох елементів
-const button = document.querySelectorAll('button');
+      for(let i = 0; i<course.length;i++){
+        //витягаємо прогресс і додаємо його
+        total+=course[i].progress;
+      }
+    }else{
+      for(let subCourse of Object.values(course)){
+        students += subCourse.length;
+         for(let i = 0; i<subCourse.length;i++){
+        //витягаємо прогресс і додаємо його
+        total+=subCourse[i].progress;
+      }
+      }
+    }
 
-button.forEach(item=>{
-  //тут ми додали третій аргумент який нам каже що ми можемо використовувати цю подію тільки один раз
-item.addEventListener("click",deleteElement,{once:true});
-});
+  }
+  return total / students;
+ }
+
+ console.log(getTotalProgressByIteration(students));
+
+
+//пошук середнього прогрессу з усього об'єкту через рекурсію
+
+ function getTotalProgressByRecursion(data){
+    if(Array.isArray(data)){
+      //перебирає кількість студентів
+      let total = 0;
+
+      for(let i = 0; i<data.length;i++){
+        //витягаємо прогресс і додаємо його
+        total+=data[i].progress;
+      }
+      return [total, data.length];
+    }else{
+      let total = [0,0];
+
+      for( let subData of Object.values(data)){
+        const subDataArr = getTotalProgressByRecursion(subData);
+        total[0]+= subDataArr[0];
+        total[1]+= subDataArr[1];
+      }
+      return total
+    }
+ }   
+const result = getTotalProgressByRecursion(students);
+console.log(result[0]/result[1]);
+
