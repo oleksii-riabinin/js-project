a7534d67f93f6374e36e55f3ab890b37d8561d04
Alexey <alexeyriabinin@gmail.com>
2026-03-18 00:04:25 +0200

    способи як додати скрипти на сторінку

--- src/js/main.js
+++ src/js/main.js
@@ -2,30 +2,18 @@ import "/src/sass/style.scss";
  "use strict"; //работаем в современном режиме
 
 
-//події на мобілці
+const p = document.querySelectorAll('p');
+console.log(p);
 
-//touchstart - при натисканні
-//touchmove - коли водимо по елементу
-//touchend - коли прибираємо палець
-//touchenter
-//touchleave
-//touchcancel
 
-window.addEventListener("DOMContentLoaded", ()=>{
 
-  const box =document.querySelector(".box");
 
-  box.addEventListener('touchmove',(e)=>{
-    e.preventDefault();
-    //тут ми отримуємо коорлинати при наведенні
-    console.log(e.targetTouches[0].pageX);
-
-    console.log(e.touches);
-    
-  });
-});
-
-//властивості
-//touches - видає нам список усіх пальців які зараз взаємодієють з екраном
-//targetTouches - видає нам список усіх пальців які зараз взаємодієють з елементом
-//changedTouches - список пальців які які беруть участь в цій події(це пальці які виконали якусь дію: наприклад коли прибрали з кнопки )
+//приклад як ще додати скрипти на сторінку
+//script.async= false; - це означає що скрипти будуть йти один за одним
+function loadScript (src){
+  const script = document.createElement('script');
+script.src=src;
+script.async= false;
+document.body.append(script);
+}
+loadScript("/src/js/main.js");
