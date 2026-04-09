351a84b20318bc8790b859adfd5a63c72c03fce7
Alexey <alexeyriabinin@gmail.com>
2026-04-09 22:23:28 +0300

    створили функції які при натисканн відкривали модальне вікно(для всіх кнопок) і додали декілька гарниї фішок

--- src/js/main.js
+++ src/js/main.js
@@ -2,141 +2,156 @@
 
 import "/src/scss/style.scss";
 
+window.addEventListener("DOMContentLoaded", () => {
+  //Tabs
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
 
-window.addEventListener("DOMContentLoaded", ()=>{
-//Tabs
-const tabs = document.querySelectorAll('.tabheader__item'),
-      tabsContent =document.querySelectorAll('.tabcontent'),
-      tabsParent = document.querySelector('.tabheader__items');
-
-//функція яка ставить для всіх елементів disply:none і проходить по кожному елементу і прибирає клас активності
-function hideTabContent(){
-  tabsContent.forEach(item =>{
-    item.classList.add('hide');
-    item.classList.remove('show','fade');
-  });
-  tabs.forEach(item =>{
-    item.classList.remove("tabheader__item_active");
-  });
-}
-
-function showTabContent(i=0){//показує нам спочатку перший елемент 
-  tabsContent[i].classList.add('show','fade');//показує елемент 
-  tabsContent[i].classList.remove('hide');
-    tabs[i].classList.add("tabheader__item_active");//додаємо клас активності
-}
-
-hideTabContent();
-showTabContent();
-
-tabsParent.addEventListener('click',(event)=>{//дія натискання
-  const target = event.target;//на який елемент натиснули
-
-  // Перевіряємо: чи ми натиснули саме на кнопку таба, а не просто десь поруч
-  if(target && target.classList.contains('tabheader__item')){
-    // Якщо так, перебираємо всі наші таби, щоб знайти порядковий номер того, на який натиснули
-    tabs.forEach((item,i)=>{
-      if(target==item){// Коли знайшли співпадіння
-        hideTabContent();// Спочатку все ховаємо
-        showTabContent(i);// Показуємо контент, номер якого (i) збігається з номером кнопки
-      }
+  //функція яка ставить для всіх елементів disply:none і проходить по кожному елементу і прибирає клас активності
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
     });
   }
-});
 
-
-//Timer
-const deadLine = '2026-05-20';
-//функція яка отримує різницю між датами
-function getTimeRemaining(endtime){
-  //отримаємо кількість мілисекунд
-  let days,hours,minutes,seconds;
-  const t = Date.parse(endtime) - Date.parse(new Date());
-  if(t<=0){
-    days=0;
-    hours=0;
-    minutes=0;
-    seconds=0;
-  }else{
-
-    days = Math.floor(t / (1000 * 60 * 60 * 24)),
-    hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-    minutes = Math.floor((t / 1000 / 60) % 60),
-    seconds = Math.floor((t / 1000 ) % 60);
+  function showTabContent(i = 0) {
+    //показує нам спочатку перший елемент
+    tabsContent[i].classList.add("show", "fade"); //показує елемент
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active"); //додаємо клас активності
   }
-  return{
-  'total':t,
-  'days':days,
-  'hours':hours,
-  'minutes':minutes,
-  'seconds': seconds
 
-  };
-}
+  hideTabContent();
+  showTabContent();
 
-function getZero(num){
-  if(num>=0 && num <10){
-    return `0${num}`;
-  }else{
-    return num;
+  tabsParent.addEventListener("click", (event) => {
+    //дія натискання
+    const target = event.target; //на який елемент натиснули
+
+    // Перевіряємо: чи ми натиснули саме на кнопку таба, а не просто десь поруч
+    if (target && target.classList.contains("tabheader__item")) {
+      // Якщо так, перебираємо всі наші таби, щоб знайти порядковий номер того, на який натиснули
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          // Коли знайшли співпадіння
+          hideTabContent(); // Спочатку все ховаємо
+          showTabContent(i); // Показуємо контент, номер якого (i) збігається з номером кнопки
+        }
+      });
+    }
+  });
+
+  //Timer
+  const deadLine = "2026-05-20";
+  //функція яка отримує різницю між датами
+  function getTimeRemaining(endtime) {
+    //отримаємо кількість мілисекунд
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      ((days = Math.floor(t / (1000 * 60 * 60 * 24))),
+        (hours = Math.floor((t / (1000 * 60 * 60)) % 24)),
+        (minutes = Math.floor((t / 1000 / 60) % 60)),
+        (seconds = Math.floor((t / 1000) % 60)));
+    }
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
   }
-}
 
-function setClock(selector,endtime){
-  const timer = document.querySelector(selector);
-
-
-
-  const days = timer.querySelector("#days"),
-    hours = timer.querySelector("#hours"),
-    minutes = timer.querySelector("#minutes"),
-    seconds = timer.querySelector("#seconds");
-
-  updateClock();
-
-  const timeInterval = setInterval(updateClock, 1000);
-
-
-function updateClock(){
-  const t = getTimeRemaining(endtime);
-  days.innerHTML = getZero(t.days);
-  hours.innerHTML = getZero(t.hours);
-  minutes.innerHTML = getZero(t.minutes);
-  seconds.innerHTML = getZero(t.seconds);
-
-  if(t.total <=0){
-    clearInterval(timeInterval);
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
   }
-}
-}
 
-setClock('.timer',deadLine);
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
 
+    const days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds");
 
-//modal
+    updateClock();
 
-const modalTrigger = document.querySelector('[data-modal]'),
-      modal = document.querySelector('.modal'),
-      modalCloseBtn = document.querySelector('[data-close]');
+    const timeInterval = setInterval(updateClock, 1000);
 
-//це працює лише не прешій кнопці
-modalTrigger.addEventListener('click',()=>{
-  modal.classList.add('show');
-  modal.classList.remove('hide');
-  // modal.classList.toggle('show'); -- або так
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
-  document.body.style.overflow = 'hidden';
-});
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
 
-modalCloseBtn.addEventListener('click',()=>{
-  modal.classList.add("hide");
-  modal.classList.remove("show");
-  document.body.style.overflow = "";
-  // modal.classList.toggle('show'); -- або так
-});
+  setClock(".timer", deadLine);
 
+  //modal
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
 
+  //це працює лише не прешій кнопці
+  // modalTrigger.addEventListener("click", () => {
+  //   modal.classList.add("show");
+  //   modal.classList.remove("hide");
+  //   // modal.classList.toggle('show'); -- або так
 
+  //   document.body.style.overflow = "hidden";
+  // });
 
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+  modalCloseBtn.addEventListener("click", closeModal);
 
+  //для всіх наших кнопок
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.classList.add("show");
+      modal.classList.remove("hide");
+      // modal.classList.toggle('show'); -- або так
+      document.body.style.overflow = "hidden";
+    });
+  });
+
+  //створили те що коли ми натискаємо на зону поруч з модальним вікном то воно теж буде закриватись
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  //створили те що коли натискаємо на Escape то модальне вікно закриваеться
+  //обробник подій який показує натискання клавіши
+  document.addEventListener("keydown", (e) => {
+    //подія в event яка показує що за кнопка натиснута
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
 });
