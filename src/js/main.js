b34bd1cd8a28877db4af47f3726edc85c5858860
Alexey <alexeyriabinin@gmail.com>
2026-04-18 22:34:27 +0300

    практика з класами,створення карток за допомогою їх

--- src/js/main.js
+++ src/js/main.js
@@ -158,17 +158,86 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  
-//функція яка відкриває модальне вікно коли ми доходимо до кінця сторінки 
-//   function showModalByScroll() {
-//     if (
-//       window.pageYOffset + document.documentElement.clientHeight >=
-//       document.documentElement.scrollHeight - 1
-//     ) {
-//       openModal();
-//     }
-    
-//   }
+  //функція яка відкриває модальне вікно коли ми доходимо до кінця сторінки
+  //   function showModalByScroll() {
+  //     if (
+  //       window.pageYOffset + document.documentElement.clientHeight >=
+  //       document.documentElement.scrollHeight - 1
+  //     ) {
+  //       openModal();
+  //     }
 
-//  window.addEventListener("scroll", showModalByScroll);
+  //   }
+
+  //  window.addEventListener("scroll", showModalByScroll);
+
+  //використовуємо класи для карток
+  //в цьому класі маємо всі наші значення та також викликаємо метод який конвертує валюту з доларів в гривні
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 43;
+      this.changeToUAN();
+    }
+    //метод конвертації валют
+    changeToUAN() {
+      this.price = this.price * this.transfer;
+    }
+    //метод який створює наші картки які приймають в себе всі наші значення 
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+              <div class="menu__item">
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+            ${this.descr}
+            </div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+          </div>
+    `;
+      //додаємо наш елементна сторінку
+      this.parent.append(element);
+    }
+  }
+  //створили об'єкт який працює тут і зараз, він зробить дії і все, більше посилань ми не будемо на нього мати
+  //він створюється без назв змінної В
+  new MenuCard(
+    "/src/img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    ".menu .container",
+  ).render();
+
+
+        new MenuCard(
+          "/src/img/tabs/elite.jpg",
+          "elite",
+          "Меню “Премиум”",
+          "В меню “Премиум” мы используем не только красивый дизайн упаковки,но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+          14,
+          ".menu .container",
+        ).render();
+
+    new MenuCard(
+      "/src/img/tabs/post.jpg",
+      "post",
+      'Меню "Постное"',
+      'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля,овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+      21,
+      ".menu .container",
+    ).render();
+
+
 });
--- src/js/test.js
+++ src/js/test.js
@@ -2,46 +2,6 @@ import "/src/scss/style.scss";
 "use strict";
 window.addEventListener("DOMContentLoaded", () => {
 
-
-  //створення класу
-class Rectangle{
-  //в конструктор передаємо значення які хочемо використовувати
-  constructor(height,width){
-    this.height= height;
-    this.width = width;
-  }
-  //метод класу 
-  calcArea(){
-    return this.height * this.width;
-  }
-}
-
-//створили наслідувальний клас,який бере значення з головного класу
-//спочатку створюємо звичайний клас
-//потім через extends вводимо назву класу який ми хочемо наслідувати
-class ColoredRectangleWithText extends Rectangle{
-  constructor(height,width,text,bgColor){
-    //метод super допомогає нам не писати властивості які є в головному класі, а просто їх використовувати 
-    super(height,width);
-    this.text = text;
-    this.bgColor =bgColor;
-  }
-showMyProps(){
-  console.log(`Текст: ${this.text}, колір: ${this.bgColor}`);
-}
-}
-//як і звичайно створюється новий об'єкт
-const div = new ColoredRectangleWithText(25,10,"hello","red");
-div.showMyProps();
-console.log(div.calcArea());
-
-//новий об'єкт зі значеннями 
-const squre = new Rectangle(10,10);
-const long = new Rectangle(20,100);
-console.log(squre.calcArea());
-console.log(long.calcArea());
-
-
 });
 
 
