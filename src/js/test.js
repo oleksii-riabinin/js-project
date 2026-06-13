c1a0e04f51842294023b12ad8f49adecc34e0c2c
Alexey <alexeyriabinin@gmail.com>
2026-06-14 00:32:21 +0300

    живі та статичні колекції

--- src/js/test.js
+++ src/js/test.js
@@ -1,39 +1,34 @@
 import "/src/scss/style.scss";
 ("use strict");
 window.addEventListener("DOMContentLoaded", () => {
-  //Оператор орцианальной цепочки(?.)
+  //Живі та статичні колекції і потрібні методи
 
-  const box = document.querySelector(".box");
-  const block = document.querySelector(".block");
-  console.log(block);
-  if(block){
-  console.log(block.textContent);
+  const boxesQuery = document.querySelectorAll(".box"); //статичні колекції
+  const boxesGet = document.getElementsByClassName("box"); //живі колекції
+
+  //можно створити звичайнийстатичний масив
+  console.log(Array.from(boxesGet));
+
+  boxesQuery[0].remove(); 
+  //показує нам відбиток, коли ми видалили перший елемент
+  //то все рівно в дереві будуть показуватись 3 ел
+  boxesGet[0].remove(); 
+  //а тут воно показує зміни, і коли ми тут ще видалили один елемент
+  //то воно покаже один елемент в дереві
+  console.log(boxesQuery);
+  console.log(boxesGet);
+  console.log(document.body.children);
+
+  for (let i = 0; i < 5; i++) {
+    const div = document.createElement("div");
+    div.classList.add("box");
+    document.body.append(div);
   }
 
-  //наш оператор проверяет значение слева от себя и если оно имеет значение null или undefined
-  //то он остонавливает операцию и возращает undefined
-  //если без него и без обычного условия то выведет ошибку
-  // через которую остольной код после нее не будет виден
-  //РАБОТАЕТ ТОЛЬКО НА ЧТЕНИЕ КОНТЕНТА,ЕСЛИ ПОПРОБОВАТЬ ВПИСАТЬ В НЕГО ЧТО-ТО ТО БУДЕТ ОШИБКА
-  block?.textContent = "123"; //БУДЕТ ОШИБКА
-  console.log(block?.textContent);
-  console.log(1 + 2);
-
-  //тут ми маємо об'єкт(наприклад не ми писали а прийшов з серверу)
-  const userData = {
-    name: "Ivan",
-    age: null,
-    say: function(){
-      console.log("Hello");
-    }
-  };
-  userData.say();
-  //перевіряємо чи існує такий метод 
-  userData.helo?.()
-    if(userData && userData.skills && userData.skills.js){
-  console.log(userData.skills.js);
-    }
-  //якщо нам потрібно щось вивезти, а ми цього не маємо в нашому об'єкті
-  //то щоб не писати великі умови ми можемо скористатися оператором орцианальной цепочки(?.)
-  console.log(userData.skills?.js);
+  boxesQuery.forEach((box) => {
+    //метод який первіряє будь якіCSS-селекторами на їх наявність(перевіряє тільки там де запитуємо)
+    if (box.matches(".this")) console.log("This one!");
+  });
+  //метод який шукає найближчого батька(процює по всему HTML)
+  console.log(boxesQuery[0].closest(".wrapper"));
 });
